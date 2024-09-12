237edebb094e3a736680a1b6b9bd64e537bec6e7
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-09-12 23:06:46 +0100

    feat: complete timeline

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -4,4 +4,5 @@ export { default as Hero } from "./Hero.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Faq } from "./Faq.astro";
 export { PoweredBy } from "./poweredBy";
+export { Timeline } from "./timeline";
 export { Judges } from "./judge";
--- src/data/index.ts
+++ src/data/index.ts
@@ -43,3 +43,44 @@ export const FAQ = [
       "We give you real time updates via app notifications at every point of your delivery process.",
   }
 ];
+
+export const timeline = [
+  {
+    date: "Friday, October 4, 2024",
+    time: "12 noon",
+    description: "Early registration deadline",
+    registrationRelated:true
+  },
+  {
+    date: "Saturday, October 5, 2024,",
+    time: "10 am - 2 pm",
+    description:
+      "Abeokuta FinHack Event Kick off the challenge and set the stage for innovation. This day is dedicated to welcoming participants, fostering learning, networking, and providing an overview of the hackathon's problem space and rules. Individuals without a team will receive support in forming one.",
+  },
+  {
+    date: "Friday, October 11, 2024",
+    time: "12 noon",
+    description:
+      "Final registration deadline Only open to newly formed teams at the Abeokuta FinHack Event. At least one of the team members must have registered as an individual before the early deadline.",
+      registrationRelated:true
+  },
+  {
+    date: "Sunday, October 27, 2024",
+    time: "11:59 pm",
+    description:
+      "Solution submission deadline The main requirement is that the team's solution must not be commercially available at the time of submission. It can be a simple prototype, a proof of concept, or a front-end application.",
+  },
+  {
+    date: "Monday, November 4, 2024",
+    description:
+      "Finalist (top 10 solutions) announcement Solutions will be judged on their originality, feasibility, design, and technical skill",
+  },
+  {
+    date: "Monday - Friday, November 4 - 8, 2024",
+    description: "Mentorship session Finalists are paired with online mentors who provide guidance and coaching to help refine their solutions and prepare a pitch deck.",
+  },
+  {
+    date: "Saturday, November 9, 2024",
+    description: "Final pitch and award event Finalists will pitch and demo their solutions to a live audience and a panel of judges. The top three teams will be selected and awarded cash prizes",
+  },
+];
