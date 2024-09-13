57f18fb9d63ef758fdac9295af5fcd9976f29e39
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-09-13 04:22:23 +0100

    feat: add about sectioin

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -3,6 +3,7 @@ export { default as Header } from "./Header.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Faq } from "./Faq.astro";
+export { default as About } from "./About.astro";
 export { PoweredBy } from "./poweredBy";
 export { Timeline } from "./timeline";
 export { Judges } from "./judge";
