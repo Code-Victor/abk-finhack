68b5247f47f7345299e7c6b8d65a4fa00b4d3598
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-09-16 20:11:13 +0100

    feat: complete placeholder page

--- src/components/inc/index.ts
+++ src/components/inc/index.ts
@@ -1,2 +1,5 @@
 export {default as AuroraBackground} from './AuroraBackground.astro';
 export {default as Countdown} from './Countdown.astro';
+export {default as BaseHead} from './BaseHead.astro';
+export {default as AnimatedGrid} from './AnimatedGrid.astro';
+export {default as ShinyText} from './ShinyText.astro';
--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Abeokuta FinHack";
-export const SITE_DESCRIPTION =
-  "Abeokuta FinHack is a local fintech event and competition designed by The Radiating Church Business & Career Network to drive innovation in the financial sector while exposing young people to world-class solution development models and processes. By bringing together innovators, developers, creatives, and industry experts, this event offers a prime opportunity to learn, network, and discover cutting-edge solutions that make financial services more secure, inclusive, and accessible for everyone.";
+export const SITE_TITLE = {
+  bcn: "The Radiating Church Business & Career Network",
+  finhack: "Abeokuta FinHack",
+};
+export const SITE_DESCRIPTION = {
+  bcn: "The Radiating Church Business & Career Network is a community of professionals, entrepreneurs, and career enthusiasts who are committed to personal and professional growth. We provide a platform for networking, mentorship, and skill development to help members achieve their career and business goals.",
+  finhack:
+    "Abeokuta FinHack is a local fintech event and competition designed by The Radiating Church Business & Career Network to drive innovation in the financial sector while exposing young people to world-class solution development models and processes. By bringing together innovators, developers, creatives, and industry experts, this event offers a prime opportunity to learn, network, and discover cutting-edge solutions that make financial services more secure, inclusive, and accessible for everyone.",
+};
 export const REGISTRATION_LINK =
   "https://docs.google.com/forms/d/e/1FAIpQLSdxb8zVDsyyWjpKdAPZeT7yayhdlPgcIpo0s3lSs1k3E_eGeA/viewform";
