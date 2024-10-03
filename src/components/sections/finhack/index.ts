40b9b6ae01f1bb3d26dc0cd79886bcb45ef65015
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-10-03 14:55:17 +0100

    feat: add event section

--- src/components/sections/finhack/index.ts
+++ src/components/sections/finhack/index.ts
@@ -3,10 +3,10 @@ export { default as Hero } from "./Hero.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Faq } from "./Faq.astro";
 export { default as About } from "./About.astro";
+export { default as Event } from "./Event.astro";
 export { PoweredBy } from "./poweredBy";
 export { Timeline } from "./timeline";
 export { Experts } from "./experts";
 export { Challenge } from "./challenge";
 export { Prizes } from "./prizes";
 export { Guidelines } from "./guidelines";
-
